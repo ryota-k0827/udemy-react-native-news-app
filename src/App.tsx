4f35c7e1a05aa0379e584b8a49cc4a72ab8af3ea
ryota-k0827 <50436249+ryota-k0827@users.noreply.github.com>
2023-02-10 23:50:06 +0900

    Clear 5-27

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,25 @@
+import { NEWS_API_KEY } from '@env';
+import axios from 'axios';
 import { StatusBar } from 'expo-status-bar';
 import { useEffect, useState } from 'react';
 import { FlatList, SafeAreaView, StyleSheet } from 'react-native';
 
 import { ListItem } from './components/ListItem';
-import dummyArticles from './dummies/articles.json';
 import { Articles } from './types/articles';
+
+const URL = `https://newsapi.org/v2/top-headlines?country=jp&category=business&apiKey=${NEWS_API_KEY}`;
 
 export default function App() {
   const [articles, setArticles] = useState<Articles>([]);
 
-  const fetchArticles = () => {
-    setArticles(dummyArticles);
+  const fetchArticles = async () => {
+    try {
+      const res = await axios.get(URL);
+      console.log(res.data.articles);
+      setArticles(res.data.articles);
+    } catch (e) {
+      console.log(e);
+    }
   };
 
   useEffect(() => {
